6654fd02a8dec15f70139b061eaa08ea89313c81
Zia Ahmed <ziaahmedf17@gmail.com>
2025-04-18 04:59:45 +0500

    Update ContentView.swift

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Stepper("\(sleepAmount.formatted()) Hours:", value: $sleepAmount, in: 4...12, step: 0.25)
         }
         .padding()
     }
