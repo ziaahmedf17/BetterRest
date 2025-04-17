c7335c4e7a2868cac65184bb1c624b538e3cf3b8
Zia Ahmed <ziaahmedf17@gmail.com>
2025-04-18 04:53:21 +0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Zi on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
